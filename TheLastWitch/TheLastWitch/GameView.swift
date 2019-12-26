f27de6d97f7dc61b3b05d9fbd3721cf5d8e9e366
Aleksandra Kustra <aleksandrakustra@gmail.com>
2019-12-26 22:24:55 +0100

    Creating base for the game

---
+++ TheLastWitch/TheLastWitch/GameView.swift
@@ -0,0 +1,35 @@
+//
+//  GameView.swift
+//  TheLastWitch
+//
+//  Created by Aleksandra Kustra on 26/12/2019.
+//  Copyright © 2019 Aleksandra Kustra. All rights reserved.
+//
+
+import SceneKit
+import SpriteKit
+
+
+// holds SpriteKit 2D UI
+class GameView: SCNView {
+
+    //MARK: lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    deinit {
+        
+    }
+    
+    //MARK: internal functions
+    
+    //MARK: D-Pad
+    
+    //MARK: attack button
+    
+}
---
+++ TheLastWitch/TheLastWitch/GameViewController.swift
@@ -0,0 +1,74 @@
+//
+//  GameViewController.swift
+//  TheLastWitch
+//
+//  Created by Aleksandra Kustra on 26/12/2019.
+//  Copyright © 2019 Aleksandra Kustra. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+enum GameState {
+    case loading
+    case playing
+}
+
+class GameViewController: UIViewController {
+
+    var gameView: GameView {
+        return view as! GameView
+    }
+    var mainScene: SCNScene!
+    
+    var gameState: GameState = .loading
+    
+    
+    //MARK: lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScene()
+        gameState = .playing
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    //MARK: scene
+    private func setupScene() {
+        gameView.allowsCameraControl = true
+        //possible deletion in case of bad fps performance
+        gameView.antialiasingMode = .multisampling2X
+        
+        mainScene = SCNScene(named: "art.scnassets/Scenes/Stage1.scn")
+        gameView.scene = mainScene
+        gameView.isPlaying = true
+    }
+    
+    //MARK: walls
+    
+    //MARK: camera
+    
+    //MARK: player
+    
+    //MARK: touches + movement
+    
+    //MARK: game loop functions
+    
+    //MARK: enemies
+
+}
+
+//MARK: extensions
