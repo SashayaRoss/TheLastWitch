c1172177ae77308b58a8bc39560672340a0d61b1
Aleksandra Kustra <aleksandrakustra@gmail.com>
2020-05-28 19:17:35 +0200

    fix npc collider, working on player animation

--- TheLastWitch/TheLastWitch/Scene/Nodes/Characters/NPC/NpcCollider.swift
+++ TheLastWitch/TheLastWitch/Scene/Nodes/Characters/NPC/NpcCollider.swift
@@ -13,19 +13,19 @@ final class NpcCollider: ColliderInterface {
 
     func setupCollider(with scale: CGFloat) -> SCNNode {
         //wyznaczenie kształtu i wielkości figury
-        let geometry = SCNCapsule(capRadius: 60, height: 200)
+        let geometry = SCNCapsule(capRadius: 1.2, height: 4)
         //ustawienie jego koloru do testów
         geometry.firstMaterial?.diffuse.contents = UIColor.blue
         
         //stworzenie collider’a na bazie figury
         collider = SCNNode(geometry: geometry)
         //określenie pozycji, nazwy i widoczności
-        collider.position = SCNVector3Make(0.0, 120, 0.0)
+        collider.position = SCNVector3Make(0.0, 2.5, 0.0)
         collider.name = "npcCollider"
         collider.opacity = 1.0
         
         //skalowanie kształtu do wielkości obiektu na scenie
-        let physicsGeometry = SCNCapsule(capRadius: 20 * scale, height: 52 * scale)
+        let physicsGeometry = SCNCapsule(capRadius: 1.2 * scale, height: 4 * scale)
         let physicsShape = SCNPhysicsShape(geometry: physicsGeometry, options: nil)
         //nadanie physicsBody
         collider.physicsBody = SCNPhysicsBody(type: .kinematic, shape: physicsShape)
